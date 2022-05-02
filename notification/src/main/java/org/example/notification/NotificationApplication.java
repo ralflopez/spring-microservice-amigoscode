94a76ceac528cebf692e70abdc39ea7de53ba3a7
Ralf <ralflopez3@gmail.com>
2022-05-02 20:55:52 +0800

    :white_check_mark: test publishing message with rabbitmq in notifications command line runner

--- notification/src/main/java/org/example/notification/NotificationApplication.java
+++ notification/src/main/java/org/example/notification/NotificationApplication.java
@@ -1,13 +1,28 @@
 package org.example.notification;
 
+import org.example.amqp.RabbitMQMessageProducer;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
 
-@SpringBootApplication
+@SpringBootApplication(
+        scanBasePackages = {
+                "org.example.notification",
+                "org.example.amqp"
+        }
+)
 @EnableEurekaClient
 public class NotificationApplication {
     public static void main(String[] args) {
         SpringApplication.run(NotificationApplication.class, args);
     }
+
+//    @Bean
+//    CommandLineRunner commandLineRunner(RabbitMQMessageProducer producer, NotificationConfig notificationConfig) {
+//        return args -> {
+//            producer.publish("foo", notificationConfig.getInternalExchange(), notificationConfig.getInternalNotificationRoutingKey());
+//        };
+//    }
 }
