1c0c7870d45313909ffd08d6a012acea1cb1e005
Ralf <ralflopez3@gmail.com>
2022-05-02 12:11:58 +0800

    :wrench: configure simple rabbit listener container factory

--- amqp/src/main/java/org/example/amqp/RabbitMQConfig.java
+++ amqp/src/main/java/org/example/amqp/RabbitMQConfig.java
@@ -2,6 +2,7 @@ package org.example.amqp;
 
 import lombok.AllArgsConstructor;
 import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
@@ -24,8 +25,18 @@ public class RabbitMQConfig {
     }
 
     @Bean
+    public SimpleRabbitListenerContainerFactory simpleRabbitListenerContainerFactory() {
+        SimpleRabbitListenerContainerFactory factory =
+                new SimpleRabbitListenerContainerFactory();
+        factory.setConnectionFactory(connectionFactory);
+        factory.setMessageConverter(jacksonConverter());
+        return factory;
+    }
+
+    @Bean
     public MessageConverter jacksonConverter() {
-        MessageConverter jackson2JsonMessageConverter = new Jackson2JsonMessageConverter();
+        MessageConverter jackson2JsonMessageConverter =
+                new Jackson2JsonMessageConverter();
         return jackson2JsonMessageConverter;
     }
 }
